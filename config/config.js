9719ae6149a7ec93192010886ffbbe29852abd2e
shon1t <sdele4@outlook.com>
2025-09-26 19:13:29 +1000

    roll back

--- config/config.js
+++ config/config.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
-
 module.exports = {
-    PORT: process.env.PORT || 3000,
-    TOKEN_SECRET: process.env.TOKEN_SECRET
+    PORT: 3000,
+    TOKEN_SECRET: "amazing-spiderman",
 }
