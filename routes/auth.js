d600f46e32e693ebb8e8784fbb6ed13d8d8dcc45
shon1t <sdele4@outlook.com>
2025-09-27 03:06:58 +1000

    random ai stuff

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { CognitoIdentityProviderClient, InitiateAuthCommand } = require("@aws-sdk/client-cognito-identity-provider");
+const {
+  CognitoIdentityProviderClient,
+  InitiateAuthCommand,
+  RespondToAuthChallengeCommand
+} = require("@aws-sdk/client-cognito-identity-provider");
 const crypto = require("crypto");
 
 const router = express.Router();
@@ -12,7 +16,6 @@ const CLIENT_ID = "e2lgatu20g780tsmitg1usn5";
 const CLIENT_SECRET = "p3190udi8jq16bd2jpgn2j74kqg807uga9hrh3qiun2bqo0c1gr";
 const USER_POOL_ID = "ap-southeast-2_LoqVf6hsi";
 
-
 // Helper to compute SECRET_HASH
 function getSecretHash(username) {
   return crypto
@@ -22,7 +25,7 @@ function getSecretHash(username) {
 }
 
 router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, newPassword } = req.body;
 
   const params = {
     AuthFlow: "USER_PASSWORD_AUTH",
@@ -38,16 +41,39 @@ router.post("/login", async (req, res) => {
     const command = new InitiateAuthCommand(params);
     const response = await cognito.send(command);
 
+    // Normal login flow
     if (response.AuthenticationResult) {
-      res.json({ authToken: response.AuthenticationResult.IdToken });
-    } else if (response.ChallengeName) {
-      res.status(401).json({
-        error: `Challenge required: ${response.ChallengeName}`,
-        details: response
-      });
-    } else {
-      res.status(401).json({ error: "Login failed, unknown response", details: response });
+      return res.json({ authToken: response.AuthenticationResult.IdToken });
     }
+
+    // Handle NEW_PASSWORD_REQUIRED
+    if (response.ChallengeName === "NEW_PASSWORD_REQUIRED") {
+      if (!newPassword) {
+        // client must provide new password on next request
+        return res.status(403).json({
+          error: "NEW_PASSWORD_REQUIRED",
+          message: "User must set a new password",
+          session: response.Session // needed to continue
+        });
+      }
+
+      const challengeResponse = new RespondToAuthChallengeCommand({
+        ClientId: CLIENT_ID,
+        ChallengeName: "NEW_PASSWORD_REQUIRED",
+        Session: response.Session,
+        ChallengeResponses: {
+          USERNAME: username,
+          NEW_PASSWORD: newPassword,
+          ...(CLIENT_SECRET ? { SECRET_HASH: getSecretHash(username) } : {})
+        }
+      });
+
+      const finalResponse = await cognito.send(challengeResponse);
+      return res.json({ authToken: finalResponse.AuthenticationResult.IdToken });
+    }
+
+    // Unknown case
+    return res.status(401).json({ error: "Login failed", details: response });
   } catch (err) {
     console.error("Cognito login error:", err);
     res.status(401).json({ error: err.message || JSON.stringify(err) });
