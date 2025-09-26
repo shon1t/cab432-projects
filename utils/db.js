44b79dbc3ae199e58007fa6dc3381d5789752b0b
shon1t <sdele4@outlook.com>
2025-09-26 15:31:43 +1000

    import query command

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-const { DynamoDBClient, PutItemCommand, UpdateItemCommand, GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, PutItemCommand, UpdateItemCommand, QueryCommand } = require("@aws-sdk/client-dynamodb");
 const { v4: uuidv4 } = require("uuid");
 
 const db = new DynamoDBClient({ region: "ap-southeast-2" });
