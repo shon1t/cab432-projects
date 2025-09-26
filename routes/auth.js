47f4e498c76f5508d251202dbf6ce84d2a6b722a
shon1t <sdele4@outlook.com>
2025-09-27 02:38:48 +1000

    change aws sdk to v3

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const AWS = require("aws-sdk");
+const crypto = require("crypto");
+
+const {
+  CognitoIdentityProviderClient,
+  InitiateAuthCommand,
+} = require("@aws-sdk/client-cognito-identity-provider");
 
 const router = express.Router();
 
-const cognito = new AWS.CognitoIdentityServiceProvider({
-  region: "ap-southeast-2"
+const cognito = new CognitoIdentityProviderClient({
+  region: "ap-southeast-2",
 });
 
 const CLIENT_ID = "e2lgatu20g780tsmitg1usn5";
-const CLIENT_SECRET = "p3190udi8jq16bd2jpgn2j74kqg807uga9hrh3qiun2bqo0c1gr"; 
+const CLIENT_SECRET = "p3190udi8jq16bd2jpgn2j74kqg807uga9hrh3qiun2bqo0c1gr";
 const USER_POOL_ID = "ap-southeast-2_LoqVf6hsi";
 
-// Helper to compute SECRET_HASH
-const crypto = require("crypto");
+// Helper to compute SECRET_HASH 
 function getSecretHash(username) {
   return crypto
     .createHmac("SHA256", CLIENT_SECRET)
@@ -20,6 +24,7 @@ function getSecretHash(username) {
     .digest("base64");
 }
 
+// Login route
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
@@ -29,14 +34,17 @@ router.post("/login", async (req, res) => {
     AuthParameters: {
       USERNAME: username,
       PASSWORD: password,
-      SECRET_HASH: getSecretHash(username)
-    }
+      SECRET_HASH: getSecretHash(username),
+    },
   };
 
   try {
-    const response = await cognito.initiateAuth(params).promise();
+    const command = new InitiateAuthCommand(params);
+    const response = await cognito.send(command);
+
     res.json({ authToken: response.AuthenticationResult.IdToken });
   } catch (err) {
+    console.error("Cognito login error:", err);
     res.status(401).json({ error: err.message || JSON.stringify(err) });
   }
 });
